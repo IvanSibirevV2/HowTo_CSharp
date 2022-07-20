8bbc6dffd9c5842f21ffaa5eb9f66f888665da40
qwe <qwe>
2022-07-20 15:20:34 +0300

    ****

--- NuGet/ConsoleApp1/ConsoleApp1/Program.cs
+++ NuGet/ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             System.Console.WriteLine("Привет мир");
-            System.Console.WriteLine(TTK.EXT_TTK.Get_TTK());
+            //System.Console.WriteLine(TTK.EXT_TTK.Get_TTK());
             System.Console.ReadLine();
         }
     }
